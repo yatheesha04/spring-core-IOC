ead869b2f6467a81e633b52ee96a18115f84a46e
B Yatheesha <byathe@publicisgroupe.net>
2020-06-01 03:44:50 +0530

    simple project using spring frame work using .xml file for creating objects

--- springcoredemo/src/main/java/com/coder/springcoredemo/App.java
+++ springcoredemo/src/main/java/com/coder/springcoredemo/App.java
@@ -1,5 +1,9 @@
 package com.coder.springcoredemo;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 /**
  * Hello world!
  *
@@ -7,7 +11,22 @@ package com.coder.springcoredemo;
 public class App 
 {
     public static void main( String[] args )
+    
     {
-        System.out.println( "Hello World!" );
+    	/*------
+    	 * add spring context  maven dependency in pom.xml 
+    	 * to use this bean factory class and getbean method
+    	 * 
+    	 * 
+    	 * to use getbean method we need factory class i.e beanfactory
+    	 * 
+    	 * bean factory is created use .xml file  
+    	 * which containes details regarding objects to be created and managed
+    	 * 
+    	 * --- */
+    	BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        Coder  coder=(Coder) factory.getBean("coder");
+        coder.code();
+        
     }
 }
---
+++ springcoredemo/src/main/java/com/coder/springcoredemo/Coder.java
@@ -0,0 +1,8 @@
+package com.coder.springcoredemo;
+
+public class Coder {
+	public void code() {
+		System.out.println("coding>>>>>>>");
+	}
+
+}
